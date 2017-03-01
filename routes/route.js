6cedacf008142a1c2f605a484e35cb0c020d6570
Carlos Chi <cchi@belizetelemedia.net>
2017-03-01 15:30:52 -0600

    Deployment changes done

--- routes/route.js
+++ routes/route.js
@@ -7,27 +7,28 @@ exports.home=function(req,res){
 
 exports.city=function(req,res){
     var cityName=req.params.city;
-    var title,heading;
+    var title,headline;
     var imageArray=[1,2,3,4];
 
     if(cityName==='belize'){
         title="Belize";
-        heading="Belize: Where i was born and ";
+        headline="Belize: Where love is in the air";
     }
     else if(cityName==='paris'){
         title="Paris";
-        heading="Paris: Good talkers are only found in Paris";
+        headline="Paris: Good talkers are only found in Paris";
     }
+
     else if(cityName==='newyork'){
         title="New York";
-        heading="New York: Come to New York to become someone new";
+        headline="New York: Come to New York to become someone new";
         imageArray.push(5);
         imageArray.push(6);
     }
 
     res.render('city',{
         title:title,
-        headline:heading,
+        headline:headline,
         city:cityName,
-        imageArray: imageArray});
+        imageArray:imageArray});
 }
